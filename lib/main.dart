7e45aaec48aa14e20d7fcb1a85298ac3c9126055
jennyjen12 <ucagjennifer2@gmail.com>
2025-08-13 12:10:29 +0800

    feat: add ClasssName
    What your Class Does
    Example usage and sample
    output

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import "person.dart";
 import "computer.dart";
 
 void main () {
-  var person = Person("Gian", 21);
+  var person = Person("Mark", 21);
   person.introduce();
 
   var computer = Computer("ASUS", "ASUS TUF A15");
   computer.describe();
+
 }
