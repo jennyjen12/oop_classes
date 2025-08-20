0720a946735b77a1cfe967edcaa7915060487eb0
jennyjen12 <ucagjennifer2@gmail.com>
2025-08-20 21:19:46 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
-import "person.dart";
-import "computer.dart";
+import 'University.dart';
+import 'Department.dart';
+import 'Student.dart';
+import 'Professor.dart';
 
+<<<<<<< Updated upstream
 void main () {
   var person = Person("Gian", 21);
   person.introduce();
 
   var computer = Computer("ASUS", "ASUS TUF A15");
   computer.describe();
+=======
+void main() {
+  var university = University("BILIRAN PROVINCE STATE UNIVERSITY", "NAVAL BILIRAN");
+  university.describe(); 
+
+  var department = Department("BSCS", "OOP");
+  department.describe();  
+
+  var student = Student("MARY ROSE", "BSCS-2C");
+  student.describe();      
+
+  var professor = Professor("STEPHEN JANSEEN DELAPEÑA BALO", "OOP");
+  professor.describe();
+>>>>>>> Stashed changes
 }
