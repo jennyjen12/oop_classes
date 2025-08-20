9f918ad7a60e1c1c8005f8cdfc8efcb00c2f5fc9
jennyjen12 <ucagjennifer2@gmail.com>
2025-08-20 21:43:56 +0800

    oop_

--- lib/computer.dart
+++ lib/computer.dart
@@ -1,12 +1,16 @@
-class Computer {
-  String brand;
-  String model;
+class University {
+  String nameofuniversity;
+  String location;
 
-  Computer(this.brand, this.model);
+  University(this.nameofuniversity, this.location);
 
   void describe() {
     // ignore: avoid_print
+<<<<<<< Updated upstream:lib/computer.dart
     print("I am $model manufactured by $brand");
+=======
+    print(" $nameofuniversity   in $location");
+>>>>>>> Stashed changes:lib/University.dart
   }
   
 }
