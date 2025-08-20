6a7f22d57ea1d330d5fa6e72406607547c546fe2
jennyjen12 <ucagjennifer2@gmail.com>
2025-08-20 21:31:59 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
-import "person.dart";
-import "computer.dart";
+import 'University.dart';
+import 'Department.dart';
+import 'Student.dart';
+import 'Professor.dart';
 
-void main () {
-  var person = Person("Mark", 21);
-  person.introduce();
+void main() {
+  var university = University("BILIRAN PROVINCE STATE UNIVERSITY", "NAVAL BILIRAN");
+  university.describe();   // <-- make sure this method exists in University class
 
-  var computer = Computer("ASUS", "ASUS TUF A15");
-  computer.describe();
+  var department = Department("BSCS", "OOP");
+  department.describe();   // <-- make sure this method exists in Department class
 
+  var student = Student("MARY ROSE", "BSCS-2C");
+  student.describe();      // <-- make sure this method exists in Student class
+
+  var professor = Professor("STEPHEN JANSEEN DELAPEÑA BALO", "OOP");
+  professor.describe();
 }
